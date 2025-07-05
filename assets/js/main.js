015c81ef53859c0b3e422b4e126f4c24e051f0b6
taponbiswas007 <taponbiswassdp@gmail.com>
2025-07-05 13:01:56 +0600

    elebate updating

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,19 @@ $(document).ready(function () {
         $(".navlink_area").slideToggle();
     })
 })
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+        // Get the header element
+        var header = $(".header");
+        // Get the scroll position
+        var scroll = $(window).scrollTop();
+        
+        // Add or remove sticky class based on scroll position
+        if (scroll >= 100) { // You can adjust this value (100px) as needed
+            header.addClass("sticky");
+        } else {
+            header.removeClass("sticky");
+        }
+    });
+});
