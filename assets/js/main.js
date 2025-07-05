eba42fb007650962ac796a942b063340a525fe09
Tapon chandra sarker <108454446+taponbiswas007@users.noreply.github.com>
2025-07-05 22:18:21 +0600

    completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,18 +4,36 @@ $(document).ready(function () {
     })
 })
 
-$(document).ready(function() {
-    $(window).scroll(function() {
+$(document).ready(function () {
+    $(window).scroll(function () {
         // Get the header element
         var header = $(".header");
         // Get the scroll position
         var scroll = $(window).scrollTop();
-        
+
         // Add or remove sticky class based on scroll position
         if (scroll >= 100) { // You can adjust this value (100px) as needed
             header.addClass("sticky");
         } else {
             header.removeClass("sticky");
         }
+    });
+});
+$(document).ready(function () {
+    // Show the button when scrolled down 100px
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.backToTopBtn').fadeIn();
+        } else {
+            $('.backToTopBtn').fadeOut();
+        }
+    });
+
+    // Scroll to top when button is clicked
+    $('.backToTopBtn').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
     });
 });
